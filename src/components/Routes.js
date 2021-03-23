a7825394266f77b105ebfabd99441d473cbc1186
Laura Aleman <79146743+lauraaleman3@users.noreply.github.com>
2021-03-23 14:57:43 -0500

    commit
    added wireframe

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,6 +3,7 @@ import Home from './Home'
 import Daily from './Daily'
 import Weekly from './Weekly'
 import Navbar from './Navbar'
+import Wireframe from './Wireframe'
 import {
   BrowserRouter as Router,
   Switch,
@@ -21,6 +22,7 @@ class Routes extends Component {
           <Route name="Home" exact path="/" component={Home} />
           <Route name="Daily" path="/daily" component={Daily} />
           <Route name="Weekly" path="/weekly" component={Weekly} />
+          <Route name="Wireframe" path="/Wireframe" component={Wireframe} />
         </div>
       </Router>
     );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './components/Routes';
-// import Footer from './components/Footer';
+
 
 
 
 
 ReactDOM.render(
   <Routes />,
-  // <Footer />,
-
   document.getElementById('root')
 );
 
